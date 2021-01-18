7dc74f0144dd68ccf15e59e9f3f532132f7435ec
Chiranjeev <sentashani@gmail.com>
2021-01-18 23:52:00 +0530

    Added getdata API for coinbase

--- lib/bandl/coinbase.py
+++ lib/bandl/coinbase.py
@@ -1,7 +1,6 @@
 import json
 import pandas as pd
 import datetime
-
 from bandl.request import RequestUrl
 from bandl.helper import get_date_range
 
@@ -15,6 +14,23 @@ class CoinbaseUrl:
         self.HEADER =   {
                         'Content-Type': 'application/json',
                         }
+        self.data_columns = ["OpenTime","Low","High","Open","Close","Volume"]
+
+    def __interval_to_garanularity(self,internal):
+        time_frame  = pd.Timedelta(internal)
+        return time_frame.total_seconds()
+
+    def get_candle_data_url(self,symbol,start,end,interval,dayfirst=False):
+        #granularity represents to total seconds
+        '''The granularity field must be one of the following values: {60, 300, 900, 3600, 21600, 86400}.
+            Otherwise, your request will be rejected. These values correspond to timeslices representing one minute,
+            five minutes, fifteen minutes, one hour, six hours, and one day, respectively.'''
+        granularity = str(int(self.__interval_to_garanularity(interval)))
+        str_start = pd.to_datetime(start,dayfirst=dayfirst).strftime('%Y-%m-%dT%H:%M:%S')
+        str_end = pd.to_datetime(end,dayfirst=dayfirst).strftime('%Y-%m-%dT%H:%M:%S')
+        url = self.BASE_URL+"/products/"+symbol+"/candles?start="+str_start+"&stop="+\
+                str_end+"&granularity="+granularity
+        return url
 
 class Coinbase:
     def __init__(self,api_key=None, api_secret=None,timeout=DEFAULT_TIMEOUT,max_retries=MAX_RETRIES):
@@ -49,7 +65,12 @@ class Coinbase:
 
             #capitalize symbol
             symbol = symbol.upper()
-            #Todo
+            data_url = self.urls.get_candle_data_url(symbol,start=s_from,end=e_till,interval=interval)
+            res = self.__request.get(data_url,headers=self.urls.HEADER)
+            data = json.loads(res.text)
+            dfs = pd.DataFrame(data,columns=self.urls.data_columns)
+            dfs['OpenTime'] = pd.to_datetime(dfs['OpenTime'], unit='s')
+            dfs.set_index("OpenTime",inplace=True)
             return dfs
 
         except Exception as err:
